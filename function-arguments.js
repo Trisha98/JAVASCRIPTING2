bf4e27ca1a2954f56927b9078591af39e3dfe837
Trisha98 <trishmusiclover@gmail.com>
2016-03-04 02:03:39 +0800

    Added files via upload

---
+++ function-arguments.js
@@ -0,0 +1,7 @@
+var first = 53;
+var sec = 61;
+var third = 67;
+function math(first,sec,third){
+		return (sec * third) + first;
+} 
+console.log(math(53,61,67));
---
+++ functions.js
@@ -0,0 +1,4 @@
+function eat (food){
+	return food + ' tasted really good.';
+} eat = eat('bananas');
+console.log(eat);
